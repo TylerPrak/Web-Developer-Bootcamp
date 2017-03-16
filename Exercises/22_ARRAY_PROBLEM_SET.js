a897856c0e0a480d46422303be09a5d985caa053
john robbins <prayforparasites@gmail.com>
2017-03-16 09:56:06 -0400

    Added parts I skipped as Unfinished Work

--- Exercises/22_ARRAY_PROBLEM_SET.js
+++ Exercises/22_ARRAY_PROBLEM_SET.js
@@ -1,16 +1,25 @@
 var numarray = [1, 2, 3];
 
+//printReverse using For Loop
 function printReverse(array) {
 	for (var i = numarray.length; i => 0; i--) {
 		console.log(numarray[i]);
 	}
 }
+
+//printReverse using forEach
+function printReverseForEach() {
+	colors	
+}
+
+
+
 /*
 var identicalArray = [1, 1, 1, 1];
 var unidenticalArray = [1, 2, 3, 4];
 
 function isUniform(array) {
 	for (var i = 1;numarray[0] !== numarray[i];i++) {
-		
+
 	}
 }*/
